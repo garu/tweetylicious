95f9134b6717bbc92e89548082c424de9ca904e9
Breno G. de Oliveira <garu@cpan.org>
2010-06-13 01:45:41 -0300

    initial take

---
+++ tweetylicious.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+#==================================================#
+# Tweetylicious! A one-file microblog application  #
+#--------------------------------------------------#
+# this file is meant as an example of how easy it  #
+# is to create cool web applications using cutting #
+# edge technology, with Perl 5, JavaScript and     #
+# just a few lines of code!                        #
+#                                                  #
+# Tweetylicious is meant as a hommage to Twitter,  #
+# a very cool micro-blogging site, but is in no    #
+# way affiliated with it. We hope this work, which #
+# is released for free as open source software     #
+# (see LICENSE in the bottom), will stimulate      #
+# all the young minds out there to create even     #
+# more amazing stuff. Viva la revolution! :)       #
+#==================================================#
+
+#--------------------------------------#
+# first we create our database (model) #
+#--------------------------------------#
+package Model;
+
+
+#-------------------------#
+# now the web application #
+#-------------------------#
+package main;
+
+use Mojolicious::Lite;
+
+
+# let's rock and roll!
+shagadelic;
+
+
+#------------------------#
+# finally, the templates #
+#------------------------#
+__DATA__
+
